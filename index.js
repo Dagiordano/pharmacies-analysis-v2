cce2f62ac8eae35b3fcfe373e6daf36c44233a04
Diego Giordano <diegogiordano@Diegos-MacBook-Air.local>
2023-11-16 16:49:59 -0300

    Feat: de añade funciones de la p4,p5,p6

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const pureFun = require('./pureFun/p1');
 const API = require('./impureFun/DFAPI');
 const Writter = require('./impureFun/WRF');
 
-const { countLocalesByCadena, countLocalesByComuna, countLocalesDespuesDeHora } = pureFun;
+const { countLocalesByCadena, countLocalesByComuna, countLocalesDespuesDeHora, countCadenasByComuna, masCadenasPorComuna, promedioHorarios } = pureFun;
 async function main() {
   try {
     const data = await API('https://andreshoward.com/pharmacies');
     const localesByCadena = countLocalesByCadena(data);
     const localesByComuna = countLocalesByComuna(data);
     const localesDespuesDeHora = countLocalesDespuesDeHora(data, '10:00:00');
+    const cadenasByComuna = countCadenasByComuna(data);
+    const presenteByComuna = masCadenasPorComuna(data);
+    const promedioHoras = promedioHorarios(data);
     await Writter('1-locales-por-cadena.json', localesByCadena);
     await Writter('2-locales-por-comuna.json', localesByComuna);
     console.log(`3-locales-abren-despues-de-hora.json: ${localesDespuesDeHora} locales abren después de las 10:00:00`);
+    await Writter('4-cadenas-por-comuna.json', cadenasByComuna);
+    await Writter('5-cadenas-mas-presentes-por-comuna.json', presenteByComuna)
+    await Writter('6-promedio-horarios-por-cadena.json', promedioHoras)
+
     console.log('Fin');
   } catch (error) {
     console.error('Error:', error);
--- pureFun/p1.js
+++ pureFun/p1.js
@@ -23,9 +23,84 @@ function countLocalesByComuna(data) {
   }
   
 
+function countCadenasByComuna(data) {
+  return data.reduce((acc,farmacia) => {
+    const comuna = farmacia.comuna_nombre.trim().toLowerCase();
+    const cadena = farmacia.local_nombre.trim().toLowerCase();
+    const id = farmacia.local_id.trim().toLowerCase();
+  
+
+    if(!acc[comuna]) {
+      acc[comuna] = {};
+    }
+    if (!acc[comuna][cadena]) {
+      acc[comuna][cadena] = [];
+    }
+    acc[comuna][cadena].push(id);
+    return acc;
+  }, {});
+}
+
+
+function masCadenasPorComuna(data) {
+  const cadenaPorComuna = countCadenasByComuna(data);
+  const objeto = {};
+  for (const comuna in cadenaPorComuna){
+    const comunas = cadenaPorComuna[comuna]
+    let maxCadena = '';
+    let maxNum = 0;
+    for (const cadena in comunas) {
+      if (comunas[cadena].length > maxNum) {
+        maxCadena = cadena;
+        maxNum = comunas[cadena].length;
+      
+      }
+      objeto[comuna] = maxCadena; 
+    }
+
+
+  }
+  return objeto;
+}
+
+
+function promedioHorarios(data){ 
+  const promedio = data.reduce((acc, {local_nombre, funcionamiento_hora_apertura, funcionamiento_hora_cierre}) => {
+    const cadena = local_nombre.trim().toLowerCase();
+    const apertura = new Date(`2020-08-03 ${funcionamiento_hora_apertura}`);
+    const cierre = new Date(`2020-08-03 ${funcionamiento_hora_cierre}`);
+    const hours = Math.abs(cierre - apertura) / 36e5; // obtener la diferencia en horas
+
+    if (!acc[cadena]) {
+      acc[cadena] = [];
+    }
+    acc[cadena].push(hours);
+    return acc;
+  }, {})
+
+    for (const local in promedio) {
+      const suma = promedio[local].reduce((a,b) => a+b,0);
+      const avg = suma/promedio[local].length;
+      promedio[local] = avg;
+
+    }
+  
+  return promedio;
+}
+
+
+
+
+
+
+
+
 
 module.exports = {
     countLocalesByCadena,
     countLocalesByComuna,
     countLocalesDespuesDeHora,
+    countCadenasByComuna,
+    masCadenasPorComuna,
+    promedioHorarios
   };
